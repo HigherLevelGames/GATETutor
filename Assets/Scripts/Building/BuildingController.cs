9194aab2f0a8eedc641fe15adc2ed66e139292d2
Jordan Nguyen <planettop92@Jordans-MacBook-Pro-3.local>
2015-02-28 15:39:54 -0700

    BuildingController and DataCell.cs
    Basic grid setup, see JordanTest/Building2D.unity Functionality not yet
    implemented

---
+++ Assets/Scripts/Building/BuildingController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingController : MonoBehaviour
+{
+	public int numCol = 5;
+	public int numRow = 3;
+	public int numInputs = 4;
+	public int numOutputs = 2;
+	public int consoleW = 0;
+
+	public GameObject Grid;
+	public GameObject Inputs;
+	public GameObject Outputs;
+
+	public int level = 0;
+	public int buildingStyle;
+	public int experience = 0;
+
+	// stats
+	int power;
+	int defense;
+	//etc.
+
+	private int w = 0; // w of cells
+	private int h = 0; // h of cells
+
+	public TextAsset feedback;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Grid = new GameObject("Grid");
+		Grid.transform.parent = this.transform;
+		Inputs = new GameObject("Inputs");
+		Inputs.transform.parent = this.transform;
+		Outputs = new GameObject("Outputs");
+		Outputs.transform.parent = this.transform;
+
+		w = (Screen.width-consoleW)/(numCol+2); // 2 for input and output
+		h = Screen.height/Mathf.Max(numRow, numInputs, numOutputs);
+		for(int i = 0; i < numRow; i++)
+		{
+			GameObject row = new GameObject("R"+i);
+			for(int j = 0; j < numCol; j++)
+			{
+				makeCell(row, "C"+(j+1), "None", new Vector2(j+1, i)); // col=x, row=y
+			}
+			row.transform.parent = Grid.transform;
+		}
+
+		for(int i = 0; i < numInputs; i++)
+		{
+			makeCell(Inputs, "I"+i, "Input", new Vector2(0, i)); // col=x, row=y
+		}
+
+		for(int i = 0; i < numOutputs; i++)
+		{
+			makeCell(Outputs, "O"+i, "Output", new Vector2(numCol+1, i)); // col=x, row=y
+		}
+	}
+
+	// makes a tagged cell for the parent
+	void makeCell(GameObject par, string name, string tag, Vector2 loc)
+	{
+		GameObject temp = new GameObject(name);
+		temp.AddComponent<DataCell>();
+		DataCell info = temp.GetComponent<DataCell>();
+		info.makeOutline(tag);
+		info.location = loc;
+		info.size = new Vector2(w, h);
+		temp.transform.parent = par.transform;
+	}
+}
---
+++ Assets/Scripts/Building/DataCell.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Reused code from StepAnalyzer.cs
+public class DataCell : MonoBehaviour
+{
+	public GameObject Inputs;
+	public GameObject Output;
+	public Vector2 size // changes based on numCol, max(numRow,numInputs,numOutputs) in BuildingController.cs
+	{
+		get
+		{
+			return (Vector2)this.transform.localScale;
+		}
+		set
+		{
+			this.transform.localScale = value;
+			this.transform.position = new Vector2(loc.x*value.x, loc.y*value.y);
+		}
+	}
+	private Vector2 loc = Vector2.zero;
+	public Vector2 location // col=x, row=y
+	{
+		get
+		{
+			return loc;
+		}
+		set
+		{
+			loc = value;
+			this.transform.position = new Vector2(loc.x*size.x, loc.y*size.y);
+		}
+	}
+	public bool available;
+	public bool isOn;
+
+	private Texture2D wireTexture;
+
+	public void makeOutline(string tag)
+	{
+		this.tag = tag;
+
+		int resolution = 256;
+		int lineThickness = 5;
+		wireTexture = new Texture2D(resolution, resolution);
+		for (int i = 0; i < resolution; i++)
+		{
+			for (int j = 0; j < resolution; j++)
+			{
+				wireTexture.SetPixel(i, j, Color.clear);
+				if (i < lineThickness || j < lineThickness || i > (resolution - 1) - lineThickness || j > (resolution-1) - lineThickness)
+				{
+					if(tag == "Input")
+					{
+						wireTexture.SetPixel(i, j, Color.blue);
+					}
+					else if(tag == "Output")
+					{
+						wireTexture.SetPixel(i, j, Color.red);
+					}
+					else
+					{
+						wireTexture.SetPixel(i, j, Color.green);
+					}
+				}
+			}
+		}
+		wireTexture.Apply();
+	}
+
+	// OnGUI() draws a green square outline
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(this.transform.position.x, this.transform.position.y, size.x, size.y), wireTexture);
+	}
+}
